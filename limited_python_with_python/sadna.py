0f6c430f58ee768e08aa459c3a2f8961546e9f12
nevemidbar17 <nevemidbar17@gmail.com>
2026-01-20 17:50:09 +0200

    Update sadna.py

--- limited_python_with_python/sadna.py
+++ limited_python_with_python/sadna.py
@@ -1,9 +1,10 @@
 from big_data_util import set_limits, print_current_memory_usage, print_size_of_object
 
 # Example usage: Limit to 20MB memory and 5 minutes execution
-set_limits(memory_mb=20, time_sec=300)
+set_limits(memory_mb=20, time_sec=180)
 
 l = [0] * 1_000_000  # This allocation exceeds the limit and crashes the process.
 
 help(print_current_memory_usage)  # Util function, remove this if not used
 help(print_size_of_object)  # Util function, remove this if not used
+
